c6e27a0f3f6a0a333bc7e477402e94377dc12e2e
shimul shil <72187771+shimulshil@users.noreply.github.com>
2021-12-17 16:56:26 +0100

    Update App.js
    size

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TODO from './component/todo';
 const App=()=> {
   return (
     <div className="App">
-      <h2 style={{marginLeft: "20px",fontSize: "20px"}}>React_Redux_Todo List</h2>
+      <h2 style={{marginLeft: "20px",fontSize: "21px"}}>React_Redux_Todo List</h2>
       <span className="title">Todo List</span> <br />
       <TODO />
     </div>
